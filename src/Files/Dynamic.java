a496d6db9ea8935e8619be87f028b026a343a890
HP <HP@DESKTOP-3AT3950>
2020-12-06 00:06:47 +0000

    Initial commit.

---
+++ src/Files/Dynamic.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Files;
+
+/**
+ *
+ * @author HP
+ */
+public class Dynamic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Connection1();
+        login t = new login ();
+        t.setVisible(true);
+        // TODO code application logic here
+    }
+
+    private static void Connection1() {
+        
+    }
+
+   
+}
